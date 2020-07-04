696b3f8b8a1fe051c0fb313f95b09f7d993d654c
yedajiang44 <602830483@qq.com>
2020-07-05 00:59:18 +0800

    添加协议版本

--- src/main/java/cn/org/hentai/simulator/jtt808/JTT808Message.java
+++ src/main/java/cn/org/hentai/simulator/jtt808/JTT808Message.java
@@ -4,8 +4,7 @@ package cn.org.hentai.simulator.jtt808;
  * Created by houcheng when 2018/7/1.
  * TODO: 此类应该作为抽象类，需要由子类来实现2013版或2019版的定义，便于编解码的分离和协议的切换和兼容
  */
-public class JTT808Message
-{
+public class JTT808Message {
     static final byte[] ZERO_LENGTH_ARRAY = new byte[0];
 
     // 消息id
@@ -23,36 +22,36 @@ public class JTT808Message
     // 消息流水号
     public int sequence;
 
+    /**
+     * 默认2013版
+     */
+    public JTT808Version version = JTT808Version.JTT808_2013;
+
     // 消息体
     public byte[] body;
 
-    public JTT808Message()
-    {
+    public JTT808Message() {
         // ...
     }
 
-    public JTT808Message(int id)
-    {
+    public JTT808Message(int id) {
         this.id = id;
         this.body = ZERO_LENGTH_ARRAY;
     }
 
-    public JTT808Message(int id, byte[] body)
-    {
+    public JTT808Message(int id, byte[] body) {
         this.id = id;
         this.body = body;
     }
 
-    public JTT808Message(int id, String sim, byte[] body)
-    {
+    public JTT808Message(int id, String sim, byte[] body) {
         this.id = id;
         this.sim = sim;
         this.body = body;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "JTT808Message{" + "id=" + id + ", sim='" + sim + '\'' + ", packetIndex=" + packetIndex + ", packetCount=" + packetCount + ", sequence=" + sequence + '}';
     }
 }
---
+++ src/main/java/cn/org/hentai/simulator/jtt808/JTT808Version.java
@@ -0,0 +1,19 @@
+package cn.org.hentai.simulator.jtt808;
+
+/**
+ * 版本
+ */
+public enum JTT808Version {
+    /**
+     * 2011版
+     */
+    JT808_2011,
+    /**
+     * 2013版
+     */
+    JTT808_2013,
+    /**
+     * 2019版
+     */
+    JTT808_2019
+}
